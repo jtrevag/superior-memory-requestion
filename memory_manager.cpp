ab08f643cd092fcb2e19b981e6debc04a657bee2
jtrevag <jtrevag@gmail.com>
2014-04-26 15:24:22 -0500

    Buddy system in progress-free is still broken

--- memory_manager.cpp
+++ memory_manager.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <stdlib.h> 
+#include <stdio.h>
 #include "process.h"
 
 using namespace std;
 
 void easy_malloc(process* processes, int num);
-void buddy_manager(process* processes, int num);
-void* my_malloc(memoryNode*, int size);
-void my_free(void *space);
+void buddy_manager(memoryNode* root, process* processes, int num);
+char* my_malloc(memoryNode* root, int size);
+void my_free(memoryNode* root, char *space);
+
+
+int first = 1;
 
 int main(){
 	int num = 50;
 	process processes[num];
 	generateProcesses(processes, num);	
 	printProcesses(processes,num);
-	easy_malloc(processes, num);
+	//easy_malloc(processes, num);
+	memoryNode *root = new memoryNode((char*) malloc(10000000), 10000000, NULL, NULL, 0);
+	buddy_manager(root, processes, num);
 }
 
 void easy_malloc(process* processes, int num){
@@ -24,7 +30,7 @@ void easy_malloc(process* processes, int num){
 		if(i % 50 == 0 && count<50){
 			cout << "Adding process: " << count << endl;
 			running[count] = processes[count];
-			running[count].space= malloc(running[count].memory * 1000);
+			running[count].space = (char*) malloc(running[count].memory * 1000);
 			count++;
 		}
 
@@ -42,17 +48,82 @@ void easy_malloc(process* processes, int num){
 	}
 }
 
-void buddy_manager(process* processes, int num){
-	
-	memoryNode *root;
-	root->start = malloc(10000000);
+void buddy_manager(memoryNode* root, process* processes, int num){
+	//NOTE: Your code is not being pushed to the server! Remember to do that. 
+	int i = 0, count = 0, removed = 0, j = 0;
+	process running[num];
+	while(removed < num){
+		if(i % 50 == 0 && count<50){
+			cout << "Adding process: " << count << endl;
+			running[count] = processes[count];
+			running[count].space= my_malloc(root, running[count].memory * 1000);
+			count++;
+			/*
+			if(count == 50){
+				removed = 50;
+			}
+			*/
+		}
 
+		for(j = 0; j<count; j++){
+			if(running[j].cycles >= 0){
+				running[j].cycles--;	
+			}
+			if(running[j].cycles == 0){
+				cout << "Removing process: " << j << endl;
+				my_free(root, running[j].space);
+				removed++;
+			}
+		}
+		i++;
+	}
 }
 
-void* my_malloc (memoryNode *root, int size) {
-	
+char* my_malloc (memoryNode* root, int size) {
+	//printf("size: %d \n", size);
+	//If there are child nodes and the current node is not occupied, visit them!
+	if(root->left != NULL && root->right != NULL && root->occupied == 0){
+			if(my_malloc(root->left, size) == NULL){
+				my_malloc(root->right, size);	
+			}
+	} else{ //we are at a leaf node
+		if(root->max_size >= size && root->max_size/2 < size && root->left == NULL && root->right == NULL && root->occupied == 0){ //if the node is good for insertion{
+			printf("address of new node: %p with size: %d and element size: %d \n", root->start, root->max_size, size);
+			root->occupied = 1;
+			return root->start;
+		} else if(!(root->max_size > size && root->max_size/2 < size) && root->max_size > 0 && root->occupied == 0) { //Grow the tree
+			root->left = new memoryNode(root->start, root->max_size/2, NULL, NULL, 0); 
+			root->right = new memoryNode(root->start + root->max_size/2, root->max_size/2, NULL, NULL, 0);
+			my_malloc(root->left, size);
+		} else{
+			return NULL;
+		}
+	}
 }
 
-void my_free (void* space) {
+void my_free (memoryNode* root, char* space) {
+	int found = 0;
+	if(root->left != NULL){
+		if(root->left->start == space && root->left->occupied == 1){
+			cout << "Removing Node @ Left" << endl;
+			root->left->start = NULL;
+			root->left = NULL;
+			found = 1;
+		}
+	} 
+	if(root->right != NULL){
+		if(root->right->start == space && root->right->occupied == 1){
+			cout << "Removing Node @ Right" << endl;
+			root->right->start = NULL;
+			root->right = NULL;
+			found = 1;
+		}	
+	}
+	if(!found && root->left != NULL && root->right != NULL){
+		my_free(root->left, space);	
+		my_free(root->right, space);
+	}
+} 
+		
+
 	
-}
--- process.h
+++ process.h
@@ -5,14 +5,33 @@ struct process{
 		int id;
 		int cycles;
 		int memory;
-		void* space;
+		char* space;
 }; 
 
 struct memoryNode{
-	void* start;
+	char* start;
 	int max_size;
 	memoryNode* left;
 	memoryNode* right;
+	int occupied;
+
+	memoryNode()
+	{
+		start = NULL;
+        max_size = 0;
+        left = NULL;
+        right = NULL;
+        occupied = 0;
+	}
+
+    memoryNode(char* init_start, int init_max_size, memoryNode* init_left, memoryNode* init_right, int init_occupied)
+    {
+        start = init_start;
+        max_size = init_max_size;
+        left = init_left;
+        right = init_right;
+        occupied = init_occupied;
+    }
 };
 
 int getAverage(int*, int);
